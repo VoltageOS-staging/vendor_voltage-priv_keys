0ece06ff005340da817d065123c93a5f50e25072
Dmitrii <bankersenator@gmail.com>
2025-10-22 11:19:28 +0000

    keys: add gmscompat_lib
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- keys.mk
+++ keys.mk
@@ -83,6 +83,8 @@ PRODUCT_CERTIFICATE_OVERRIDES += \
     ServiceUwbResources:com.android.uwb.resources.certificate.override \
     ServiceWifiResources:com.android.wifi.resources.certificate.override \
     WifiDialog:com.android.wifi.dialog.certificate.override
+    GmscompatLib:gmscompat_lib.certificate.override \
 
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/voltage-priv/keys/testkey
 PRODUCT_EXTRA_RECOVERY_KEYS :=
+PRODUCT_CERTIFICATE_GMSCOMPAT_LIB := vendor/voltage-priv/keys/gmscompat_lib
